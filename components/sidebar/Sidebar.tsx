ce89d8500e72ad67fb86b0a3d90965c1489c6bf9
david <davidojo.dev@gmail.com>
2026-01-03 15:09:12 +0100

    Refactor sidebar and settings menu for improved  responsive UX

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { logo } from "@/assets/svgs";
 import { Button } from "../ui/button";
+import { AnimatePresence, motion } from "framer-motion";
 import {
   RiHeartFill,
   RiHome3Fill,
+  RiSettings3Fill,
   RiShoppingBagFill,
   RiShoppingCartFill,
 } from "react-icons/ri";
@@ -18,6 +20,11 @@ import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { logoFull } from "@/assets/images";
 import { X } from "lucide-react";
 
+interface SIdeBar {
+  open: boolean;
+  setOpen: (v: boolean) => void;
+}
+
 export type ActiveTab =
   | "Home"
   | "Cart"
@@ -26,28 +33,33 @@ export type ActiveTab =
   | "Settings"
   | null;
 
-interface SidebarItem {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+interface SidebarItem<P = any> {
   id: ActiveTab;
   icon: IconType;
   label: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  content?: React.ComponentType<any>;
+  props?: P;
 }
-
-interface SIdeBar {
-  open: boolean;
-  setOpen: (v: boolean) => void;
-}
-
-const sidebarItems: SidebarItem[] = [
-  { id: "Home", icon: RiHome3Fill, label: "Home" },
-  { id: "Cart", icon: RiShoppingCartFill, label: "Cart" },
-  { id: "Orders", icon: RiShoppingBagFill, label: "Orders" },
-  { id: "Favourite", icon: RiHeartFill, label: "Favourite" },
-];
 
 const Sidebar = ({ open, setOpen }: SIdeBar) => {
   const [activeTab, setActiveTab] = useState<ActiveTab>("Home");
+  const [isMobile, setIsMoble] = useState(false);
   const { setActiveCategory } = useCategory();
   const router = useRouter();
+
+  useEffect(() => {
+    const checkScreen = () => {
+      const mobile = window.innerWidth < 768;
+      setIsMoble(mobile);
+    };
+
+    checkScreen();
+    window.addEventListener("resize", checkScreen);
+
+    return () => window.removeEventListener("resize", checkScreen);
+  }, []);
 
   const handleTabChange = (tabId: ActiveTab) => {
     const active = tabId === activeTab ? null : tabId;
@@ -59,123 +71,178 @@ const Sidebar = ({ open, setOpen }: SIdeBar) => {
     router.push("/");
   };
 
+  const sidebarItems: SidebarItem[] = [
+    { id: "Home", icon: RiHome3Fill, label: "Home" },
+    { id: "Cart", icon: RiShoppingCartFill, label: "Cart" },
+    { id: "Orders", icon: RiShoppingBagFill, label: "Orders" },
+    { id: "Favourite", icon: RiHeartFill, label: "Favourite" },
+    {
+      id: "Settings",
+      icon: RiSettings3Fill,
+      label: "Settings",
+      content: SettingsMenu,
+      props: { setActiveTab },
+    },
+  ];
+
   return (
-    <aside
-      className={`md:sticky fixed z-40 left-0 inset-y-0 w-full max-w-[375px] md:max-w-sidebar shrink-0 bg-white border-r border-gray-100 transform transition-all duration-300 overflow-hidden ${
-        open ? "max-md:translate-x-0" : "max-md:-translate-x-full"
-      }`}
-    >
-      <div className="flex flex-col md:items-center py-4 px-6">
-        {/* Logo Large Screens*/}
-        <Link
-          href="/"
-          onClick={() => setActiveCategory(null)}
-          className="size-6 overflow-hidden mb-8 max-md:hidden"
-        >
-          <img src={logo.src} alt="Cargoland Food" className="object-cover" />
-        </Link>
-        {/* Logo Small Screens*/}
-        <div className="md:hidden flex justify-between items-start">
-          <Link
-            href="/"
-            onClick={() => setActiveCategory(null)}
-            className="h-10 w-[140px] block overflow-hidden mb-8"
-          >
-            <img
-              src={logoFull.src}
-              alt="Cargoland Food"
-              className="object-cover"
-            />
-          </Link>
-          <button onClick={() => setOpen(false)}>
-            <X className="size-4" />
-          </button>
-        </div>
+    <>
+      {/* Large Screens */}
+      {!isMobile && (
+        <aside className="hidden md:block sticky left-0 inset-y-0 w-sidebar shrink-0 bg-white border-r border-gray-100">
+          <div className="flex flex-col items-center py-4">
+            {/* Logo */}
+            <Link
+              href="/"
+              onClick={() => setActiveCategory(null)}
+              className="size-6 flex items-center justify-center rounded-sm bg-black overflow-hidden mb-8"
+            >
+              <img
+                src={logo.src}
+                alt="Cargoland Food"
+                className="object-cover"
+              />
+            </Link>
 
-        {/* Navigation Items */}
-        <nav className="flex flex-col gap-12 md:gap-7 flex-1">
-          {sidebarItems.map((item, idx) => {
-            const IconComponent = item.icon;
-            const isActive = activeTab === item.id;
+            {/* Navigation Items */}
+            <nav className="flex flex-col gap-7 flex-1">
+              {sidebarItems.map((item, idx) => {
+                const IconComponent = item.icon;
+                const isActive = activeTab === item.id;
+                // const Content = item.content;
 
-            return (
-              <>
-                {/* Separate home button */}
-                {idx === 0 && (
-                  <Button
-                    onClick={() => {
-                      handleCLick(item.id);
-                      setActiveCategory(null);
-                    }}
-                    variant="ghost"
-                    className="flex gap-2 items-center self-start p-0! hover:bg-transparent"
-                    aria-label={item.label}
-                  >
-                    <div
-                      className={`relative size-10 md:size-6 rounded-sm transition-colors flex justify-center items-center max-md:rounded-full max-md:bg-primary-50  ${
-                        isActive && "md:bg-gray-100"
-                      }`}
-                    >
-                      <IconComponent
-                        className={`w-5 h-5 transition-colors text-primary ${
-                          isActive ? "md:text-primary" : "md:text-gray-300"
-                        } `}
-                      />
-                    </div>
-                    <span className=" md:hidden text-xl font-medium leading-7">
-                      {item.label}
-                    </span>
-                  </Button>
-                )}
-
-                {/* Rest of the buttons with popups */}
-                {idx > 0 && (
-                  <Popover
-                    open={isActive}
-                    onOpenChange={() => handleTabChange(item.id)}
-                    key={idx}
-                  >
-                    <div
-                      key={item.id}
-                      className="flex gap-2 items-center"
-                      onClick={() => handleTabChange(item.id)}
-                    >
-                      <PopoverTrigger
-                        className={`relative size-10 md:size-6 rounded-sm transition-colors flex justify-center items-center max-md:rounded-full max-md:bg-primary-50  ${
-                          isActive && "md:bg-gray-100"
+                return (
+                  <>
+                    {/* Separate home button */}
+                    {idx === 0 && (
+                      <Button
+                        onClick={() => handleCLick(item.id)}
+                        variant="ghost"
+                        className={`relative size-6 rounded-sm transition-colors flex justify-center items-center ${
+                          isActive && "bg-gray-100"
                         }`}
                       >
                         <IconComponent
-                          className={`w-5 h-5 transition-colors max-md:text-primary ${
-                            isActive ? "md:text-primary" : "md:text-gray-300"
-                          }`}
+                          className={`w-5 h-5 transition-colors ${
+                            isActive ? "text-primary" : "text-gray-300"
+                          } `}
                         />
-                        {isActive && (
-                          <span className="absolute left-8 transform top-1/2 -translate-y-1/2 z-30 text-white py-1 px-3 bg-primary rounded-xl text-xs whitespace-nowrap pointer-events-none">
-                            {item.label}
-                          </span>
-                        )}
-                      </PopoverTrigger>
-                      <span className="md:hidden text-xl font-medium leading-7">
-                        {item.label}
-                      </span>
-                    </div>
-                    <PopoverContent
-                      side="right"
-                      // sideOffset={8}
-                      className="md:w-[374px] rounded-xl max-h-[95vh] overflow-auto hide-scrollbar px-4 shadow"
-                    ></PopoverContent>
-                  </Popover>
-                )}
-              </>
-            );
-          })}
-        </nav>
+                      </Button>
+                    )}
 
-        {/* Settings - Bottom */}
-        <SettingsMenu activeTab={activeTab} handleTabChange={handleTabChange} />
-      </div>
-    </aside>
+                    {/* Rest of the buttons with popups */}
+                    {idx > 0 && (
+                      <Popover
+                        open={isActive}
+                        onOpenChange={() => handleTabChange(item.id)}
+                        key={idx}
+                      >
+                        <PopoverTrigger
+                          key={item.id}
+                          // variant="ghost"
+                          onClick={() => handleTabChange(item.id)}
+                          className={`relative size-6 rounded-sm transition-colors flex justify-center items-center ${
+                            isActive && "bg-gray-100"
+                          }`}
+                        >
+                          <IconComponent
+                            className={`
+                    "w-5 h-5 transition-colors",
+                   ${isActive ? "text-primary" : "text-gray-300"}
+                  `}
+                          />
+                          {isActive && (
+                            <span className="absolute left-8 transform top-1/2 -translate-y-1/2 z-30 text-white py-1 px-3 bg-primary rounded-xl text-xs whitespace-nowrap pointer-events-none">
+                              {item.label}
+                            </span>
+                          )}
+                        </PopoverTrigger>
+                        <PopoverContent
+                          side="right"
+                          sideOffset={8}
+                          className="w-[374px] rounded-xl max-h-[95vh] overflow-auto hide-scrollbar px-4 shadow"
+                        >
+                          {item.content && <item.content {...item.props} />}
+                        </PopoverContent>
+                      </Popover>
+                    )}
+                  </>
+                );
+              })}
+            </nav>
+          </div>
+        </aside>
+      )}
+
+      {/* Mobile Screens */}
+      <AnimatePresence>
+        {open && (
+          <motion.aside
+            initial={{ x: "-100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "-100%" }}
+            transition={{ type: "tween", ease: "easeOut", duration: 0.2 }}
+            className="md:hidden fixed inset-0 pt-10 px-6 bg-white z-35 "
+          >
+            <div className="flex items-start justify-between">
+              <Link
+                href="/"
+                onClick={() => setActiveCategory(null)}
+                className="h-10 w-[140px] block overflow-hidden mb-8"
+              >
+                <img
+                  src={logoFull.src}
+                  alt="Cargoland Food"
+                  className="object-cover"
+                />
+              </Link>
+              <button onClick={() => setOpen(false)}>
+                <X className="size-4" />
+              </button>
+            </div>
+
+            <div className="grid gap-6">
+              {sidebarItems.map((item, idx) => {
+                const isActive = activeTab === item.id;
+                return (
+                  <div
+                    onClick={() => handleTabChange(item.id)}
+                    className="flex items-center gap-2 hover:cursor-pointer"
+                    key={idx}
+                  >
+                    <div className="size-10 flex items-center justify-center bg-primary-50 rounded-full">
+                      <item.icon className="size-6 text-primary" />
+                    </div>
+                    <p className="text-xl font-medium leading-7">
+                      {item.label}
+                    </p>
+                    <AnimatePresence>
+                      {item.content && isActive && (
+                        <motion.aside
+                          initial={{ x: "-100%" }}
+                          animate={{ x: 0 }}
+                          exit={{ x: "-100%" }}
+                          transition={{
+                            type: "tween",
+                            ease: "easeOut",
+                            duration: 0.2,
+                          }}
+                          className="md:hidden fixed inset-0 pt-10 px-6 bg-white z-35 "
+                        >
+                          <div className="fixed inset-0 p-6 z-40 bg-white">
+                            <item.content {...item.props} />
+                          </div>
+                        </motion.aside>
+                      )}
+                    </AnimatePresence>
+                  </div>
+                );
+              })}
+            </div>
+          </motion.aside>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
