a6d739cae2c0fd23702e0f09268abe21b857f263
david <davidojo.dev@gmail.com>
2026-01-07 15:43:24 +0100

    make header search ui responsive according to path

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { bell, icon, logout } from "@/assets/svgs";
+import { bell } from "@/assets/svgs";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
@@ -14,7 +14,7 @@ import { useSession } from "@/lib/hooks/useSession";
 import useAuthFlow from "@/lib/stores/authFlowStore";
 import { AnimatePresence, motion } from "framer-motion";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
 import {
   RiMapPinFill,
@@ -25,6 +25,7 @@ import {
   RiUser3Fill,
   RiArrowRightSLine,
   RiMenu4Line,
+  RiArrowLeftLine,
 } from "react-icons/ri";
 import Notifications from "./Notifications";
 import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
@@ -40,8 +41,10 @@ export function Header({ setSideBar }: HeaderProps) {
   const [screen, setScreen] = useState<MenuScreen>("root");
   const [search, setSearch] = useState("");
   const router = useRouter();
+  const path = usePathname();
   const OpenAuth = useAuthFlow((s) => s.openAuth);
   const { user: session, isAuthenticated, signOut } = useSession();
+  console.log("PathName:", path);
 
   const [firstName, lastName] = session?.fullName.split(" ") || [];
   const initials =
@@ -49,10 +52,12 @@ export function Header({ setSideBar }: HeaderProps) {
       lastName &&
       `${firstName.charAt(0)}${lastName.charAt(0)}`.toUpperCase()) ||
     "";
-  // console.log("FirstName:", firstName);
-  // console.log("LastName:", lastName);
-  // console.log("Initials:", initials);
-  // console.log("Data is: ", session);
+
+  const SEARCH_ROUTES = ["/", "/restaurants", "/groceries", "/markets"];
+  const BACK_ROUTES = ["/restaurants", "/groceries", "/markets"];
+
+  const matchRoutes = (routes: string[], path: string) =>
+    routes.some((route) => path === route || path.startsWith(route + "/"));
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -62,9 +67,20 @@ export function Header({ setSideBar }: HeaderProps) {
     setSearch("");
   };
 
+  const handleBack = () => {
+    if (window.history.length > 1) {
+      router.back();
+    } else {
+      router.push("/");
+    }
+  };
+
+  const showSearch = matchRoutes(SEARCH_ROUTES, path);
+  const showBack = matchRoutes(BACK_ROUTES, path);
+
   // session.signOut();
   return (
-    <header className="sticky top-0 z-30 px-4 md:px-6 py-2.25 bg-white border-b border-gray-100">
+    <header className="sticky top-0 z-30 px-4 md:px-6 md:py-2.25 max-md:pt-2.25 bg-white md:border-b border-gray-100">
       <div className="max-md:flex justify-between gap-2">
         {/* mobile menu button */}
         <Button
@@ -219,19 +235,33 @@ export function Header({ setSideBar }: HeaderProps) {
           </div>
         </div>
       </div>
-      <form
-        onSubmit={handleSubmit}
-        className="md:hidden relative mt-4 flex-1 md:max-w-[416px]"
-      >
-        <RiSearchLine className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
-        <input
-          type="text"
-          placeholder="Search for food, restaurants...."
-          className="w-full pl-10 pr-4 py-2 border-2 border-gray-200 rounded-lg text-sm placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-primary focus:bg-white transition"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </form>
+
+      {showSearch && (
+        <div className="flex gap-3 mt-4 items-center md:hidden">
+          {showBack && (
+            <button
+              onClick={handleBack}
+              className="gap-4 ml-2.5 text-sm hover:cursor-pointer"
+            >
+              <RiArrowLeftLine className="size-5" />
+            </button>
+          )}
+
+          <form
+            onSubmit={handleSubmit}
+            className="relative flex-1 md:max-w-[416px]"
+          >
+            <RiSearchLine className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
+            <input
+              type="text"
+              placeholder="Search for food, restaurants...."
+              className="w-full pl-10 pr-4 py-2 border-2 border-gray-200 rounded-lg text-sm placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-primary focus:bg-white transition"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </form>
+        </div>
+      )}
     </header>
   );
 }
