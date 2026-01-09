fbcc5a17be568adfedc4672e4b9f65ad2c06a04c
david <davidojo.dev@gmail.com>
2026-01-09 17:26:01 +0100

    make coupon success modal responsive

--- components/orders/CouponSuccessModal.tsx
+++ components/orders/CouponSuccessModal.tsx
@@ -1,6 +1,12 @@
 import { memo } from "react";
 import React from "react";
-import { Dialog, DialogClose, DialogContent, DialogFooter, DialogTitle } from "../ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogTitle,
+} from "../ui/dialog";
 import success from "@/assets/gifs/success.gif";
 import { Button } from "../ui/button";
 
@@ -9,18 +15,17 @@ interface CouponSuccessProps {
   onOpenChange: (close: boolean) => void;
 }
 
-
 const CouponSuccessModal = ({ open, onOpenChange }: CouponSuccessProps) => {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="px-7 max-w-[400px]! gap-0">
-        <div className="size-[180px] self-center justify-self-center ">
+      <DialogContent className="max-w-[400px]! max-sm:w-[95vw] gap-0 p-0 px-6 sm:px-7 pt-12 sm:py-4.5 pb-11 m-09">
+        <div className="size-[124px] sm:size-[180px] self-center justify-self-center">
           <img src={success.src} alt="coupon added " className="size-full " />
         </div>
-        <DialogTitle className="text-center font-bold text-2xl leading-8 mt-1">
+        <DialogTitle className="text-center font-bold text-2xl leading-8 mt-3">
           Coupon Added
         </DialogTitle>
-        <DialogFooter >
+        <DialogFooter>
           <DialogClose className="mt-[52px] w-full">
             <Button className="submit-btn">Done</Button>
           </DialogClose>
