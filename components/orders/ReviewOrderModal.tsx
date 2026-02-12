03495594bbee75faf2ba934c8f23383f50b9860d
david <davidojo.dev@gmail.com>
2026-02-12 22:13:31 +0100

    Add comment text box to review order modal

--- components/orders/ReviewOrderModal.tsx
+++ components/orders/ReviewOrderModal.tsx
@@ -22,9 +22,9 @@ const ReviewOrderModal = ({
   vendorId = "cbijscd6",
 }: LikeOrderProps) => {
   const [rating, setRating] = useState(0);
-  // const [msg, setMsg] = useState("");
+  const [comment, setcomment] = useState("");
   const { mutate: submitRevuew, isPending } = useSubmitReview();
-  // const maxLength = 200;
+  const maxLength = 200;
 
   const handleSubmitReview = () => {
     if (!vendorId || rating === 0) return;
@@ -32,8 +32,7 @@ const ReviewOrderModal = ({
     const payload = {
       vendorId: "08503157-05af-4d34-a5e5-fe9bf061dcf0",
       rating,
-      comment:
-        "Okay, da food waz lovely. Yaaass! Bomboclat spicy hot ponmo got me tongue ah burning. Looveeeit!!  ",
+      comment,
     };
 
     console.log("payload :", payload);
@@ -42,6 +41,7 @@ const ReviewOrderModal = ({
       onSuccess: () => {
         // onOpenChange(false);
         setRating(0); // Reset for next time
+        setcomment("");
       },
     });
   };
@@ -58,7 +58,7 @@ const ReviewOrderModal = ({
           </DialogDescription>
         </DialogHeader>
 
-        <div className="mt-4 flex justify-center">
+        <div className="mt-3 sm:mt-4 flex justify-center">
           <StarInput
             value={rating}
             onChange={setRating}
@@ -67,23 +67,23 @@ const ReviewOrderModal = ({
           />
         </div>
 
-        {/* <div className="relative mt-6">
+        <div className="relative mt-6">
           <textarea
-            value={msg}
-            onChange={(e) => setMsg(e.target.value)}
-            className="p-3 w-full rounded-xl border border-neutral-300 h-[136px] sm:h-[167px] min-h-[66px] max-h-[136px] sm:max-h-[167px] hide-scrollbar"
-            placeholder="Write here"
+            value={comment}
+            onChange={(e) => setcomment(e.target.value)}
+            className="p-3 w-full rounded-xl border border-neutral-300 h-[136px] sm:h-[135pxpx] min-h-[66px] max-h-[136px] sm:max-h-[135px] hide-scrollbar"
+            placeholder="Add a Comment"
             maxLength={maxLength}
           ></textarea>
 
           <span className="absolute right-3 bottom-3 text-neutral-500 text-xs font-medium">
-            {msg.length}/{maxLength}
+            {comment.length}/{maxLength}
           </span>
-        </div> */}
+        </div>
 
         <Button
           onClick={handleSubmitReview}
-          className={`submit-btn h-11! mt-6 mb-[37px] ${
+          className={`submit-btn h-11! mt-4.5 mb-[37px] ${
             isPending ? "bg-primary" : "disabled:bg-neutral-300"
           } `}
           disabled={isPending || rating === 0}
