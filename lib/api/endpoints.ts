f2d9ceba4b3fbfedef806efeca7d6ec7081bdb19
david <davidojo.dev@gmail.com>
2026-08-06 17:44:25 +0100

    feat(tracking): add user visits tracking

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -19,6 +19,7 @@ export const API_ROUTES = {
   user: {
     user: (id: string) => `/users/${id}`,
     updatePersonalInfo: "/users/personal-info",
+     visitor: "/users/visitor",
   },
 
   vendor: {
